9a7cdaadccce2c2ae8446df19c0ff36dfe21d2c6
Cooper Van Kampen <coopervk@gmail.com>
2017-11-12 22:09:19 -0600

    If there's JS, replace the player with the button

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,3 +2,6 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+//Replace the mp3 player native to the browser with a button, doorbell-button
+$("#doorbell-player").replaceWith('<button id="doorbell-button">Play a doorbell tune~</audio>');
