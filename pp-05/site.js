844341e61ae87ddc8a8875f02b15a9b6aa56270a
Cooper Van Kampen <coopervk@gmail.com>
2017-11-12 22:14:45 -0600

    Fix eslint errors and warnings

--- pp-05/site.js
+++ pp-05/site.js
@@ -3,17 +3,17 @@ function ringDoorbell() {
   doorbell.play();
 }
 
-//Replace the mp3 player native to the browser with a button, doorbell-button
+// Replace the mp3 player native to the browser with a button, doorbell-button
 $("#doorbell-player").replaceWith('<button id="doorbell-button">Play a doorbell tune~</audio>');
 
-//When doorbell-button is clicked, ring the doorbell
+// When doorbell-button is clicked, ring the doorbell
 $("#doorbell-button").click(function() {
   ringDoorbell();
 });
 
-//100 corresponds to d button, https://www.w3.org/2002/09/tests/keys-cancel2.html
+// 100 corresponds to d button, https://www.w3.org/2002/09/tests/keys-cancel2.html
 $(document).keypress(function(key) {
-  if(key.which == 100) {
+  if(key.which === 100) {
     ringDoorbell();
   }
 });
