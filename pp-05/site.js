2f3fc82d86225e5009fa2c1c006624e08a893825
Cooper Van Kampen <coopervk@gmail.com>
2017-11-13 21:06:58 -0600

    Fix ESLint error: make jQuery self-executing

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,19 +1,28 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+// Release the dollar-sign from jQuery's control
+$.noConflict();
 
-// Replace the mp3 player native to the browser with a button, doorbell-button
-$("#doorbell-player").replaceWith('<button id="doorbell-button">Play a doorbell tune~</audio>');
+// Re-introduce dollar sign scoped to self-executing
+// function
+(function($) {
 
-// When doorbell-button is clicked, ring the doorbell
-$("#doorbell-button").click(function() {
-  ringDoorbell();
-});
-
-// 100 corresponds to d button, https://www.w3.org/2002/09/tests/keys-cancel2.html
-$(document).keypress(function(key) {
-  if(key.which === 100) {
-    ringDoorbell();
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
   }
-});
+
+  // Replace the mp3 player native to the browser with a button, doorbell-button
+  $("#doorbell-player").replaceWith('<button id="doorbell-button">Play a doorbell tune~</audio>');
+
+  // When doorbell-button is clicked, ring the doorbell
+  $("#doorbell-button").click(function() {
+    ringDoorbell();
+  });
+
+  // 100 corresponds to d button, https://www.w3.org/2002/09/tests/keys-cancel2.html
+  $(document).keypress(function(key) {
+    if(key.which === 100) {
+      ringDoorbell();
+    }
+  });
+
+})(jQuery);
